4c918f30c2e045f949042b844148149d62e8de23
mariiapetrova92 <32820604+mariiapetrova92@users.noreply.github.com>
2017-10-16 18:32:22 +0200

    add all files

---
+++ send.php
@@ -0,0 +1,22 @@
+﻿<?php
+//Принимаем постовые данные
+$name=$_POST['name'];
+$phone=$_POST['phone'];
+$what=$_POST['what'];
+$question=$_POST['question'];
+
+
+//Тут указываем на какой ящик посылать письмо
+$to = "mashap310@gmail.com";
+//Далее идет тема и само сообщение
+$subject = "Заявка с сайта";
+$message = "Письмо отправлено из моей формы<br />
+Пользователь хочет: ".htmlspecialchars($what)."<br />
+Имя: ".htmlspecialchars($name)."<br />
+Телефон: ".htmlspecialchars($phone)."<br />
+Вопрос: ".htmlspecialchars($question);
+$headers = "From: petrovka.ru <no-reply@mysite.ru>\r\nContent-type: text/html; charset=UTF-8 \r\n";
+mail ($to, $subject, $message, $headers);
+header('Location: thanx.html');
+exit;
+?>
